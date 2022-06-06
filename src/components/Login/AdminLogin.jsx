4f33aded13645daf855b40cfce2e38207cd350c1
Rohan Shahi <rohanshahi516@gmail.com>
2022-06-06 20:49:15 +0545

    Tried implementing authentication for admin

---
+++ src/components/Login/AdminLogin.jsx
@@ -0,0 +1,166 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import login from "../../assets/images/login.png";
+import * as Yup from "yup";
+import { useFormik } from "formik";
+import axios from "axios";
+import useAuth from "../../customHooks/useAuth";
+
+export default function AdminLogin() {
+  const { setAuth } = useAuth();
+
+  const navigate = useNavigate();
+
+  // const handleSubmit = () => {
+  //   setAuth({user:"Student", pwd: "password" , roles:"student", accessToken:"qdniuy&2312nndouqaid23"});
+  //   localStorage.setItem("token","qdniuy&2312nndouqaid23");
+  //   navigate("/dashboard");
+  // }
+
+  //Initial Formik value
+
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .email("Enter valid email")
+      .required("Email is required"),
+    password: Yup.string("Password not valid")
+      .min("6", "Minimum 6 chars required")
+      .required("Password is required"),
+  });
+
+  const {
+    values,
+    touched,
+    errors,
+    handleBlur,
+    handleChange,
+    resetForm,
+    handleSubmit,
+  } = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: async (values) => {
+      let res = await axios.post(
+        "https://result-management-system-v1.herokuapp.com/api/admin/login",
+        values
+      );
+      if (res?.data?.data) {
+        const adminData = res?.data?.data;
+        setAuth({
+          user: "Admin",
+          email: adminData.email,
+          pwd: adminData.password,
+          isAdmin: adminData.isAdmin,
+          accessToken: adminData.password,
+        });
+        localStorage.setItem("token", adminData.password);
+        resetForm();
+        navigate("/dashboard");
+      } else {
+        alert("Enter valid credentials");
+        resetForm();
+      }
+    },
+  });
+
+  return (
+    <>
+      <div className="login-wrapper">
+        {/* <!-- blue circle background --> */}
+        <div className="d-none d-md-block ball login bg-primary bg-gradient position-absolute rounded-circle"></div>
+
+        {/* <!-- logo name --> */}
+        <div className="position-absolute top-0 start-0 p-3">
+          <Link to="/" className="text-decoration-none fw-bold fs-5">
+            RMS HOME
+          </Link>
+        </div>
+
+        {/* <!-- Login Section --> */}
+        <div className="container login__form active">
+          <div className="row vh-100 w-100 align-self-center">
+            <div className="col-12 col-lg-6 col-xl-6 px-5">
+              <div className="row vh-100">
+                <div className="col align-self-center p-5 w-100">
+                  <h3 className="fw-bolder">WELCOME ADMIN!</h3>
+                  <p className="fw-lighter fs-6">
+                    Are you student ?
+                    <span id="signUp" className="text-primary">
+                      <Link to="/login-student">Here </Link>
+                    </span>
+                  </p>
+                  {/* <!-- form login section --> */}
+                  <form action="" className="mt-5" onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                      <label htmlFor="username" className="form-label">
+                        Email
+                      </label>
+                      <input
+                        type="email"
+                        className="form-control text-indent shadow-sm bg-grey-light border-0 rounded-pill fw-lighter fs-7 p-3"
+                        placeholder="name@example.com"
+                        name="email"
+                        value={values.email}
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                      />
+                      {touched.email && errors.email ? (
+                        <small className="text-danger ms-2">
+                          {errors.email} *
+                        </small>
+                      ) : null}
+                    </div>
+                    <div className="mb-3">
+                      <label htmlFor="username" className="form-label">
+                        Password
+                      </label>
+                      <div className="d-flex position-relative">
+                        <input
+                          type="password"
+                          autoComplete="on"
+                          className="form-control text-indent auth__password shadow-sm bg-grey-light border-0 rounded-pill fw-lighter fs-7 p-3"
+                          name="password"
+                          value={values.password}
+                          onBlur={handleBlur}
+                          onChange={handleChange}
+                        />
+                        {/* <span className="password__icon text-primary fs-4 fw-bold bi bi-eye-slash"></span> */}
+                      </div>
+                      {touched.password && errors.password ? (
+                        <small className="text-danger ms-2">
+                          {errors.password} *
+                        </small>
+                      ) : null}
+                    </div>
+                    <div className="col text-center">
+                      <button
+                        type="submit"
+                        className="btn btn-dark btn-lg rounded-pill mt-4 w-100"
+
+                        // onClick={handleSubmit}
+                      >
+                        Login
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+            <div className="d-none d-lg-block col-lg-6 col-xl-6 p-5">
+              <div className="row vh-100 p-5">
+                <div className="col align-self-center text-center">
+                  <img src={login} className="bounce" alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/components/Student/AddStudent.jsx
@@ -0,0 +1,207 @@
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+export default function AddStudent() {
+  const initialValues = {
+    studentName: "",
+    email: "",
+    password: "",
+    address: "",
+    phone: "",
+    course: "",
+    semester: "",
+  };
+
+  const validationSchema = Yup.object().shape({
+    studentName: Yup.string("Student Name must be string").required(
+      "Student name is required"
+    ),
+    email: Yup.string().email("Enter valid email").required("Email is required"),
+    password: Yup.string("Password not valid")
+      .min("6", "Minimum 6 chars required")
+      .required("Password is required"),
+    address: Yup.string("Address must be string").required(
+      "Address is required"
+    ),
+    phone: Yup.string()
+      .min(10, "Enter valid number")
+      .required("Phone number is required"),
+    course: Yup.string().required("Please select course"),
+    semester: Yup.string().required("Please select Semester"),
+  });
+
+  const { values, touched, errors, handleBlur, handleChange, resetForm,handleSubmit } =
+    useFormik({
+      initialValues,
+      validationSchema,
+      onSubmit: (values) => {
+          console.log(values)
+      },
+    });
+
+//   console.log(errors,"errors")
+
+  return (
+    <>
+      <h3>Add Students</h3>
+
+      <form className="row g-3 mt-4" onSubmit={handleSubmit}>
+        <div className="col-md-6">
+          <label htmlFor="studentName" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="studentName"
+            name="studentName"
+            placeholder="eg : Rohan Shahi"
+            value={values.studentName}
+            onBlur={handleBlur}
+            onChange={handleChange}
+          />
+          {touched.studentName && errors.studentName ? (
+            <small className="text-danger ms-2">{errors.studentName} *</small>
+          ) : null}
+        </div>
+        <div className="col-md-6">
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            name="email"
+            id="email"
+            placeholder="eg: rohanshahi516@gmail.com"
+            value={values.email}
+            onBlur={handleBlur}
+            onChange={handleChange}
+          />
+           {touched.email && errors.email ? (
+            <small className="text-danger ms-2">{errors.email} *</small>
+          ) : null}
+        </div>
+        <div className="col-md-6">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            name="password"
+            id="password"
+            value={values.password}
+            onBlur={handleBlur}
+            onChange={handleChange}
+          />
+           {touched.password && errors.password ? (
+            <small className="text-danger ms-2">{errors.password} *</small>
+          ) : null}
+        </div>
+        <div className="col-md-6">
+          <label htmlFor="address" className="form-label">
+            Address
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="address"
+            placeholder="eg: Kalopul"
+            name="address"
+            value={values.address}
+            onBlur={handleBlur}
+            onChange={handleChange}
+          />
+           {touched.address && errors.address ? (
+            <small className="text-danger ms-2">{errors.address} *</small>
+          ) : null}
+        </div>
+
+        <div className="col-md-6">
+          <label htmlFor="phone" className="form-label">
+            Phone
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="phone"
+            name="phone"
+            value={values.phone}
+            onBlur={handleBlur}
+            onChange={handleChange}
+          />
+           {touched.phone && errors.phone ? (
+            <small className="text-danger ms-2">{errors.phone} *</small>
+          ) : null}
+        </div>
+        <div className="col-md-6">
+          <label htmlFor="course" className="form-label">
+            Course
+          </label>
+          <select
+            id="course"
+            name="course"
+            className="form-select"
+            defaultValue="BCA"
+            value={values.course}
+            onBlur={handleBlur}
+            onChange={handleChange}
+          >
+            {/* <option selected>Choose...</option> */}
+            <option>BCA</option>
+            <option>CSIT</option>
+
+            <option>BIT</option>
+          </select>
+          {touched.course && errors.course ? (
+            <small className="text-danger ms-2">{errors.course} *</small>
+          ) : null}
+        </div>
+        <div className="col-md-6">
+          <label htmlFor="semester" className="form-label">
+            Semester
+          </label>
+          <select
+            id="semester"
+            name="semester"
+            className="form-select"
+            defaultValue="1st"
+            value={values.semester}
+            onBlur={handleBlur}
+            onChange={handleChange}
+          >
+            {/* <option selected>Choose Semeseter...</option> */}
+            <option>1st</option>
+            <option>2nd</option>
+
+            <option>3rd</option>
+            <option>4th</option>
+            <option>5th</option>
+            <option>6th</option>
+            <option>7th</option>
+            <option>8th</option>
+          </select>
+          {touched.semester && errors.semester ? (
+            <small className="text-danger ms-2">{errors.semester} *</small>
+          ) : null}
+        </div>
+
+        <div className="col-12">
+          <button type="submit" className="btn btn-primary">
+            Add Student
+          </button>
+
+          <button
+            type="reset"
+            onClick={resetForm}
+            className="btn btn-primary ms-2"
+          >
+            Reset
+          </button>
+        </div>
+      </form>
+    </>
+  );
+}
